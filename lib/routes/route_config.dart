171970f13d3579396e963152e745e0282c35ccf6
chillley <chillley@163.com>
2021-11-23 22:16:48 +0800

    更改路由注入文件

--- lib/routes/route_config.dart
+++ lib/routes/route_config.dart
@@ -1,13 +1,11 @@
 import 'package:get/get.dart';
-import 'package:misty_master/pages/discover/discover_binding.dart';
-import 'package:misty_master/pages/discover/discover_page.dart';
+
 
 import 'package:misty_master/pages/index/index_page.dart';
 import 'package:misty_master/pages/main/main_binding.dart';
 import 'package:misty_master/pages/index/index_binding.dart';
-
-import 'package:misty_master/pages/mine/mine_page.dart';
 import 'package:misty_master/pages/mine/mine_binding.dart';
+import 'package:misty_master/pages/discover/discover_binding.dart';
 
 import 'package:misty_master/pages/search/search_binding.dart';
 import 'package:misty_master/pages/search/search_page.dart';
@@ -39,17 +37,12 @@ class RouteConfig {
     GetPage(
       name: index,
       page: () => IndexPage(),
-      bindings: [IndexBinding(), MainBinding()],
-    ),
-    GetPage(
-      name: discover,
-      page: () => const DiscoverPage(),
-      binding: DiscoverBinding(),
-    ),
-    GetPage(
-      name: mine,
-      page: () => const MinePage(),
-      binding: MineBinding(),
+      bindings: [
+        IndexBinding(),
+        MainBinding(),
+        DiscoverBinding(),
+        MineBinding()
+      ],
     ),
     GetPage(
       name: search,
